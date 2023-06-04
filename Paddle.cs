46e095540ad641db462b85cb26ea083091874ed0
Raj Patil <floatraj@gmail.com>
2023-06-04 23:03:11 +0530

    added method to parse webhook and get obj in return

--- Paddle.cs
+++ Paddle.cs
@@ -1,20 +1,25 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Loggernow.Paddle.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Org.BouncyCastle.Crypto;
 using Org.BouncyCastle.OpenSsl;
 using Org.BouncyCastle.Security;
 using PhpSerializerNET;
 using System.ComponentModel.Design;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
+using System.Web;
 
 namespace Loggernow.Paddle.Payments
 {
-    public class Paddle
+    public partial class Paddle
     {
         private readonly string _publicKey;
         private readonly ILogger _logger;
 
-        public Paddle(string publicKey,  ILogger logger = null)
+        private HttpRequest _req;
+
+        public Paddle(string publicKey,  ILogger logger )
         {
             _publicKey = publicKey;
             _logger = logger;
@@ -27,14 +32,14 @@ namespace Loggernow.Paddle.Payments
         /// <returns>Result of vereification.True if succeeds ,false if it fails</returns>
         public bool VerifySignature( HttpRequest req)
         {
-
-            string[] sortedKeys = req.Form.Keys.OrderBy(x => x, StringComparer.Ordinal).ToArray();
-            byte[] signature = Convert.FromBase64String(req.Form["p_signature"]);
+            _req=req;
+            string[] sortedKeys = _req.Form.Keys.OrderBy(x => x, StringComparer.Ordinal).ToArray();
+            byte[] signature = Convert.FromBase64String(_req.Form["p_signature"]);
             SortedDictionary<string, object> padStuff = new SortedDictionary<string, object>();
 
             foreach (string key in sortedKeys)
             {
-                string value = req.Form[key].ToString();
+                string value = _req.Form[key].ToString();
                 if (key != "p_signature")
                 {
                     padStuff.Add(key, value);
@@ -42,19 +47,41 @@ namespace Loggernow.Paddle.Payments
             }
             string serializedData = PhpSerialization.Serialize(padStuff);
 
-            return verifySignature(signature, serializedData, _publicKey);
-           
-        }
-        private bool verifySignature(byte[] signatureBytes, string message, string publicKey)
-        {
-            StringReader publicKeyStringReader = new StringReader(publicKey);
-            AsymmetricKeyParameter publicKeyAsym= (AsymmetricKeyParameter)new PemReader(publicKeyStringReader).ReadObject();
+            StringReader publicKeyStringReader = new StringReader(_publicKey);
+            AsymmetricKeyParameter publicKeyAsym = (AsymmetricKeyParameter)new PemReader(publicKeyStringReader).ReadObject();
             ISigner sig = SignerUtilities.GetSigner("SHA1withRSA");
             sig.Init(false, publicKeyAsym);
-            byte[] messageBytes = Encoding.UTF8.GetBytes(message);
+            byte[] messageBytes = Encoding.UTF8.GetBytes(serializedData);
             sig.BlockUpdate(messageBytes, 0, messageBytes.Length);
-            return sig.VerifySignature(signatureBytes);
+            return sig.VerifySignature(signature);
+
         }
+
+        /// <summary>
+        /// takes in optional <c>HttpRequest</c>  param if not already provided and return PaddleWebhook Obj.
+        /// </summary>
+        /// <param name="req"></param>
+        /// <returns>PaddleWebhook model</returns>
+        /// <exception cref="Exception">Throws error if HttpRequest filed is not set in class</exception>
+        public PaddleWebhook ParsePaddleWebhook(HttpRequest? req)
+        {
+            if (req == null && _req!=null)
+            {
+                return new PaddleWebhook(_req);
+            }
+            else if(req!=null && req!=null)
+            {
+                return new PaddleWebhook(req);
+            }
+            else
+            {
+                _logger.LogError("HttpRequest field was not initialised in Paddle class");
+                throw new Exception("HttpRequest not initialised");
+            }
+        }
+
+
+         
 
 
 
--- README.md
+++ README.md
@@ -9,8 +9,17 @@ This package provides abstraction to paddle webhooks and apis.
 This package exposes below methods to interact with paddle
 
 - VerifySignature
+This method takes in HttpRequest and returns if paddle webhook is valid or not
 ```
 Paddle p=new Paddle(your_public_key_string);
-bool result=p.VerifySignature(Httprequest recived);
+bool result=p.VerifySignature(HttpRequest recived);
 ```
+
+- ParsePaddleWebhook
+This method takes in potional parameter of paddle HttpRequest if it wad not already provided while verifying webhook signature earlier and parses the data and return PaddleWebhook with initialised filds.
 ```
+Paddle p=new Paddle(your_public_key_string);
+bool result=p.ParsePaddleWebhook(HttpRequest recived);
+```
+This also throws error ```HttpRequest not initialised``` if you dont pass in ```HttpRequest``` while calling this function and you have not provided ```HttpRequest``` before.
+
